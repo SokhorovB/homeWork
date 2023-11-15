e847215574ba482cc246c708bcc9ba69d65c1639
SokhorovB <112692279+SokhorovB@users.noreply.github.com>
2023-11-15 09:50:14 +0300

    add text

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -7,8 +7,9 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
+        String text = "https://selenide.org";
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("[id=search]").shouldHave(text(text));
     }
 }
