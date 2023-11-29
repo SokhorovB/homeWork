d8771497b9ab5b99b1642a893e6f6cc85fdef904
Sokhorov Basang <basanya91@yandex.ru>
2023-11-29 13:56:21 +0300

    home work Java

---
+++ src/test/java/DataType.java
@@ -0,0 +1,34 @@
+public class DataType {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 100;
+        double c = 10.1;
+        double d = 10.1;
+        byte e = 126;
+        byte f = 100;
+        boolean g = true;
+        boolean h = false;
+
+        System.out.println("Сложение: " + (a + b));
+        System.out.println("Вычитание: " + (a - c));
+        System.out.println("Деление" + ":" + " " + (a / b));
+        System.out.println("Умножение: " + (a * b));
+        System.out.println("Сравнение: " + (a > b));
+        System.out.println("Остаток от деления: " + (a % b));
+
+        Long maxLong = Long.MAX_VALUE;
+        Long sum = maxLong + 15;
+        short aShort = 32_767;
+        short bShort = 1;
+        short sumShort = (short) (aShort + bShort);
+
+        System.out.println("Переполнение MAX_VALUE: " + sum);
+        System.out.println("Переполнение Short: " + sumShort);
+
+        if (a > b) {
+            System.out.println("False");
+        } else {
+            System.out.println("True");
+        }
+    }
+}
